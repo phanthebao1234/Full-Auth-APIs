e67e54dd458b19def8ca6246d65e967227c146ac
phanthebao1234 <phanthebao888@gmail.com>
2024-11-12 21:54:40 +0700

    OAuth set up

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from djoser.social.views import ProviderAuthView
 from django.conf import settings
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -9,6 +10,34 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+class CustomProviderAuthView(ProviderAuthView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        
+        if response.status_code == 201:
+            access_token = response.data.get('token')
+            refresh_token = response.data.get('refresh')
+            
+            response.set_cookie(
+                "access",
+                access_token,
+                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+            )
+
+            response.set_cookie(
+                "refresh",
+                refresh_token,
+                max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+            )
+        return response
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
