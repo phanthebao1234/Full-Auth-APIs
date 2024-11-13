6ff58bb4e26dd238fc0db786229185254931c573
phanthebao1234 <phanthebao888@gmail.com>
2024-11-13 21:48:15 +0700

    update UserAccount add info user

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
-
+from django.utils import timezone 
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, password=None, **kwargs):
@@ -37,6 +37,9 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
         max_length=255,
         unique=True,
     )
+    date_of_birth = models.DateField(null=True, blank=True, default=timezone.now) 
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True) 
+    date_joined = models.DateTimeField(default=timezone.now) 
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -49,3 +52,8 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+    def has_perm(self, perm, obj=None): 
+        return self.is_superuser 
+    
+    def has_module_perms(self, app_label): 
+        return self.is_superuser
--- users/views.py
+++ users/views.py
@@ -13,31 +13,32 @@ from rest_framework_simplejwt.views import (
 class CustomProviderAuthView(ProviderAuthView):
     def post(self, request, *args, **kwargs):
         response = super().post(request, *args, **kwargs)
-        
+
         if response.status_code == 201:
-            access_token = response.data.get('token')
+            access_token = response.data.get('access')
             refresh_token = response.data.get('refresh')
-            
-            response.set_cookie(
-                "access",
-                access_token,
-                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
-                path=settings.AUTH_COOKIE_PATH,
-                secure=settings.AUTH_COOKIE_SECURE,
-                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
-            )
 
             response.set_cookie(
-                "refresh",
-                refresh_token,
-                max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+                'access',
+                access_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE,
+                samesite=settings.AUTH_COOKIE_SAMESITE
             )
+            response.set_cookie(
+                'refresh',
+                refresh_token,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE
+            )
+
         return response
+
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
@@ -50,7 +51,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         response.set_cookie(
             "access",
             access_token,
-            max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+            max_age=settings.AUTH_COOKIE_MAX_AGE,
             path=settings.AUTH_COOKIE_PATH,
             secure=settings.AUTH_COOKIE_SECURE,
             httponly=settings.AUTH_COOKIE_HTTP_ONLY,
@@ -60,7 +61,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         response.set_cookie(
             "refresh",
             refresh_token,
-            max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+            max_age=settings.AUTH_COOKIE_MAX_AGE,
             path=settings.AUTH_COOKIE_PATH,
             secure=settings.AUTH_COOKIE_SECURE,
             httponly=settings.AUTH_COOKIE_HTTP_ONLY,
@@ -84,7 +85,7 @@ class CustomTokenRefreshView(TokenRefreshView):
             response.set_cookie(
                 "access",
                 access_token,
-                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                max_age=settings.AUTH_COOKIE_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
